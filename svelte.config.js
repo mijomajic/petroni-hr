ee551e42510aa64f1539606f49c676ed268c2cc6
mijomajic <zoezedone@gmail.com>
2026-07-15 15:09:20 +0200

    Allow CorvusPay callback origins

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,15 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
-	kit: { adapter: adapter({ runtime: 'nodejs22.x' }) }
+	kit: {
+		adapter: adapter({ runtime: 'nodejs22.x' }),
+		csrf: {
+			trustedOrigins: [
+				'https://wallet.test.corvuspay.com',
+				'https://wallet.corvuspay.com'
+			]
+		}
+	}
 };
 
 export default config;
